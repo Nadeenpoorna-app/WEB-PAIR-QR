a211d62103acc3ee981af51d41462e28b3df2620
Nadeen Poorna <nadeenpoorna405@gmail.com>
2025-05-18 12:33:24 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,19 +1,78 @@
-const { exec } = require("child_process");
-const { upload } = require('./mega');
+const { makeid } = require('./gen-id');
 const express = require('express');
-let router = express.Router()
+const QRCode = require('qrcode');
+const fs = require('fs');
+let router = express.Router();
 const pino = require("pino");
-
-let { toBuffer } = require("qrcode");
-
-const path = require('path');
-
-const fs = require("fs-extra");
-
-const { Boom } = require("@hapi/boom");
-
-const MESSAGE = process.env.MESSAGE ||  `
-✅ *𝗦𝗘𝗦𝗦𝗜𝗢𝗡 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗬* 🚀
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers,
+    jidNormalizedUser
+} = require("@whiskeysockets/baileys");
+const { upload } = require('./mega');
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+router.get('/', async (req, res) => {
+    const id = makeid();
+ //   let num = req.query.number;
+    async function GIFTED_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/' + id);
+        try {
+var items = ["Safari"];
+function selectRandomItem(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+var randomItem = selectRandomItem(items);
+            
+            let sock = makeWASocket({
+                	
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+            
+            sock.ev.on('creds.update', saveCreds);
+            sock.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect,
+                    qr
+                } = s;
+              if (qr) await res.end(await QRCode.toBuffer(qr));
+                if (connection == "open") {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    let rf = __dirname + `/temp/${id}/creds.json`;
+                    function generateRandomText() {
+                        const prefix = "3EB";
+                        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                        let randomText = prefix;
+                        for (let i = prefix.length; i < 22; i++) {
+                            const randomIndex = Math.floor(Math.random() * characters.length);
+                            randomText += characters.charAt(randomIndex);
+                        }
+                        return randomText;
+                    }
+                    const randomText = generateRandomText();
+                    try {
+                        const { upload } = require('./mega');
+                        const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+                        let md = "𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=" + string_session;
+                        let code = await sock.sendMessage(sock.user.id, { text: md });
+                        let desc = `🚀 *𝗦𝗘𝗦𝗦𝗜𝗢𝗡 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗬* ✅
 
 > 🚫ᴅᴏɴ'ᴛ ꜱʜᴀʀᴇ ᴛʜɪꜱ ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ!!!
 
@@ -31,257 +90,79 @@ https://youtube.com/@NADEEN-MD
 
 > ▄︻デռǟɖɛɛռ-ʍɖ══━一
 
-🎯 *Nα∂єєη м∂ ву Nα∂єєη Pσσяηα* 🎯`
-if (fs.existsSync('./auth_info_baileys')) {
+🎯 *Nα∂єєη м∂ ву Nα∂єєη Pσσяηα* 🎯`;
+                        await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: "NADEEN-MD",
+thumbnailUrl: "https://files.catbox.moe/3mvn78.png",
+sourceUrl: "https://whatsapp.com/channel/0029VagN2qW3gvWUBhsjcn3I",
+mediaType: 1,
+renderLargerThumbnail: true
+}  
+}
+},
+{quoted:code })
+                    } catch (e) {
+                            let ddd = sock.sendMessage(sock.user.id, { text: e });
+                            let desc = `🚀 *𝗦𝗘𝗦𝗦𝗜𝗢𝗡 𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗬* ✅
 
-    fs.emptyDirSync(__dirname + '/auth_info_baileys');
+> 🚫ᴅᴏɴ'ᴛ ꜱʜᴀʀᴇ ᴛʜɪꜱ ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ!!!
 
-  };
+✨ *Gɪᴠᴇ ᴀ Sᴛᴀʀ ᴛᴏ Rᴇᴘᴏ Fᴏʀ Cᴏᴜʀᴀɢᴇ* 🌟
+https://github.com/Nadeenpoorna-app/NADEEN-MD
 
-  
+🪀 *Fᴏʟʟᴏᴡ Wʜᴀᴛꜱᴀᴘᴘ Cʜᴀɴɴᴇʟ* 🪀
+https://whatsapp.com/channel/0029VagN2qW3gvWUBhsjcn3I
 
-  router.get('/', async (req, res) =>  {
+👨🏻‍💻 *Cᴏɴᴛᴀᴄᴛ Oᴡɴᴇʀ* 👨🏻‍💻
+https://wa.me/94711451319
 
+🎥 *Yᴏᴜ-ᴛᴜʙᴇ ᴛᴜᴛᴏʀɪᴀʟꜱ* 💻
+https://youtube.com/@NADEEN-MD
 
+> ▄︻デռǟɖɛɛռ-ʍɖ══━一
 
-  const { default: SuhailWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, } = require("@whiskeysockets/baileys");
-
-  const store = useMultiFileAuthState({ logger: pino().child({ level: 'silent', stream: 'store' }) })
-
-  async function SUHAIL() {
-
-    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
-
-    try {
-
-      let Smd =SuhailWASocket({ 
-
-        printQRInTerminal: false,
-
-        logger: pino({ level: "silent" }), 
-
-        browser: Browsers.macOS("Desktop"),
-
-        auth: state 
-
-        });
-
-
-
-
-
-      Smd.ev.on("connection.update", async (s) => {
-
-        const { connection, lastDisconnect, qr } = s;
-
-        if (qr) {
-
-                    // Ensure the response is only sent once
-
-                    if (!res.headersSent) {
-
-                        res.setHeader('Content-Type', 'image/png');
-
-                        try {
-
-                            const qrBuffer = (await toBuffer(qr));  // Convert QR to buffer
-
-                            res.end(qrBuffer);  // Send the buffer as the response
-
-                            return; // Exit the function to avoid sending further responses
-
-                        } catch (error) {
-
-                            console.error("Error generating QR Code buffer:", error);
-
-                            
-
-                            return; // Exit after sending the error response
-
-                        }
-
+🎯 *Nα∂єєη м∂ ву Nα∂єєη Pσσяηα* 🎯`;
+                            await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: "NADEEN-MD",
+thumbnailUrl: "https://files.catbox.moe/3mvn78.png",
+sourceUrl: "https://whatsapp.com/channel/0029VagN2qW3gvWUBhsjcn3I",
+mediaType: 2,
+renderLargerThumbnail: true,
+showAdAttribution: true
+}  
+}
+},
+{quoted:ddd })
                     }
-
-        }
-
-
-
-
-
-        if (connection == "open"){
-
-          await delay(3000);
-
-          let user = Smd.user.id;
-
-
-
-
-
-//===========================================================================================
-
-//===============================  SESSION ID    ===========================================
-
-//===========================================================================================
-
-
-
-          function randomMegaId(length = 6, numberLength = 4) {
-
-                      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-                      let result = '';
-
-                      for (let i = 0; i < length; i++) {
-
-                      result += characters.charAt(Math.floor(Math.random() * characters.length));
-
-                        }
-
-                       const number = Math.floor(Math.random() * Math.pow(10, numberLength));
-
-                        return `${result}${number}`;
-
-                        }
-
-
-
-                        const auth_path = './auth_info_baileys/';
-
-                        const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
-
-
-
-                        const string_session = mega_url.replace('https://mega.nz/file/', '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=');
-
-
-
-                        const Scan_Id = string_session;
-
-          console.log(`
-
-====================  SESSION ID  ==========================                   
-
-SESSION-ID ==> ${Scan_Id}
-
--------------------   SESSION CLOSED   -----------------------
-
-`)
-
-
-
-
-
-          let msgsss = await Smd.sendMessage(user, { text:  Scan_Id });
-
-          await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
-
-          await delay(1000);
-
-          try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
-
-
-
-
-
-        }
-
-
-
-        Smd.ev.on('creds.update', saveCreds)
-
-
-
-        if (connection === "close") {            
-
-            let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-
-            // console.log("Reason : ",DisconnectReason[reason])
-
-            if (reason === DisconnectReason.connectionClosed) {
-
-              console.log("Connection closed!")
-
-             // SUHAIL().catch(err => console.log(err));
-
-            } else if (reason === DisconnectReason.connectionLost) {
-
-                console.log("Connection Lost from Server!")
-
-            //  SUHAIL().catch(err => console.log(err));
-
-            } else if (reason === DisconnectReason.restartRequired) {
-
-                console.log("Restart Required, Restarting...")
-
-              SUHAIL().catch(err => console.log(err));
-
-            } else if (reason === DisconnectReason.timedOut) {
-
-                console.log("Connection TimedOut!")
-
-             // SUHAIL().catch(err => console.log(err));
-
-            }  else {
-
-                console.log('Connection closed with bot. Please run again.');
-
-                console.log(reason)
-
-              await delay(5000);
-
-              exec('pm2 restart qasim');
-
-              process.exit(0)
-
+                    await delay(10);
+                    await sock.ws.close();
+                    await removeFile('./temp/' + id);
+                    console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ✅ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...`);
+                    await delay(10);
+                    process.exit();
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10);
+                    GIFTED_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "❗ Service Unavailable" });
             }
-
-          }
-
-
-
-
-
-
-
-      });
-
-    } catch (err) {
-
-        console.log(err);
-
-        exec('pm2 restart qasim');
-
-       await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
-
-       
-
+        }
     }
-
-  }
-
-  SUHAIL().catch(async(err) => {
-
-    console.log(err)
-
-    await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
-
-    exec('pm2 restart qasim');
-
-
-
-
-
-    //// MADE WITH 
-
-
-
+    await GIFTED_MD_PAIR_CODE();
 });
-
-return await SUHAIL()
-
-
-
-  });
-
-module.exports = router
+setInterval(() => {
+    console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
+    process.exit();
+}, 180000); //30min
+module.exports = router;
