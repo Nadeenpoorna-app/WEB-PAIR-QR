a75cd88ef27d143f13ec7aecfc6d9ee6d045fc2d
Nadeen Poorna <nadeenpoorna405@gmail.com>
2026-03-01 21:19:35 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ import { Storage } from 'megajs';
 
 const auth = {
     email: 'nadeenpoorna70@gmail.com',
-    password: '@123Nadeen',
+    password: '@123Nadeenx',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
